6085044d0cdf2be71efc17ee1747cd1593f85706
19niel <kungfayla@gmail.com>
2024-08-25 19:59:17 +0800

    Sidebar Done commit

--- watchTower-admin/client/src/scenes/layout/index.jsx
+++ watchTower-admin/client/src/scenes/layout/index.jsx
@@ -1,14 +1,26 @@
-import React, {userState} from 'react';
+import React, {useState} from 'react';
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from 'react-router-dom'; // allows to have template layouts
 import { useSelector } from "react-redux"; 
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)"); // true all false boolean for mobile screens and desktop screens
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return (
-  <Box width="100%" height="100%"> {/* Box is a material ui component that allows you to pass in properties like css properties */}
+  <Box display={isNonMobile ? "flex": "block"} width="100%" height="100%"> {/* Box is a material ui component that allows you to pass in properties like css properties */}
+     <Sidebar
+      isNonMobile={isNonMobile}
+      drawerWidth="250px"
+      isSidebarOpen={isSidebarOpen}
+      setIsSidebarOpen={setIsSidebarOpen}
+      />
      <Box>
-      <Navbar /> {/* Navbar will always exist */}
+      <Navbar 
+        isSidebarOpen={isSidebarOpen} // for closing the sidebar using the menu icon
+        setIsSidebarOpen={setIsSidebarOpen}
+      /> {/* Navbar will always exist */}
       <Outlet /> {/* Outlet represents the components underneath*/}
      </Box>  
 
