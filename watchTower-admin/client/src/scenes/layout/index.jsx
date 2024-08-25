abfdfad40714a3839a2f2473b184131addf31166
19niel <kungfayla@gmail.com>
2024-08-25 18:46:42 +0800

    Search Bar commit

--- watchTower-admin/client/src/scenes/layout/index.jsx
+++ watchTower-admin/client/src/scenes/layout/index.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {userState} from 'react';
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from 'react-router-dom'; // allows to have template layouts
+import { useSelector } from "react-redux"; 
+import Navbar from "components/Navbar";
 
 const Layout = () => {
-  return <div>Layout</div>;
-  
+  return (
+  <Box width="100%" height="100%"> {/* Box is a material ui component that allows you to pass in properties like css properties */}
+     <Box>
+      <Navbar /> {/* Navbar will always exist */}
+      <Outlet /> {/* Outlet represents the components underneath*/}
+     </Box>  
+
+  </Box>
+  );
 };
 
 export default Layout;
