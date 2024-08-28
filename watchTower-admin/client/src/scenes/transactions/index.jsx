59421f8a05d0705b6a390e5cbd93bec9108ebf36
19niel <kungfayla@gmail.com>
2024-08-29 01:24:52 +0800

    Walang FlexBetween commit

--- watchTower-admin/client/src/scenes/transactions/index.jsx
+++ watchTower-admin/client/src/scenes/transactions/index.jsx
@@ -1,112 +1,110 @@
-import React, { useState} from 'react';
-import {Box, useTheme} from "@mui/material";
+import React, { useState } from 'react';
+import { Box, useTheme } from "@mui/material";
 import { DataGrid } from '@mui/x-data-grid';
 import { useGetTransactionsQuery } from 'state/api';
 import Header from "components/Header";
-import DataGridCustomToolbar from "components/DataGridCustomToolbar.jsx";
-
-
-
+import DataGridCustomToolbar from "components/DataGridCustomToolbar"; // Correct import path
 
 const Transactions = () => {
     const theme = useTheme();
 
-    //es to be sent to the backend
+    // States to be sent to the backend
     const [page, setPage] = useState(0);
     const [pageSize, setPageSize] = useState(20);
     const [sort, setSort] = useState({});
-    const {search, setSearch} = useState("");
+    const [search, setSearch] = useState(""); // Now properly handling search state
+    const [searchInput, setSearchInput] = useState("");
 
-    const {data, isLoading} = useGetTransactionsQuery({
+    const { data, isLoading } = useGetTransactionsQuery({
         page,
         pageSize,
         sort: JSON.stringify(sort),
-        search, // params that is sending to the backend
-   
+        search, // Params that is sent to the backend
     });
-    
+
     const columns = [
-    {
-      field: "_id",
-      headerName:"ID",
-      flex: 1,
-    },
-    {
-      field: "userId",
-      headerName:"User ID",
-      flex: 1,
-    },
-    {
-      field: "createdAt",
-      headerName:"Created At",
-      flex: 1,
-    },
-    
-    {
-      field: "products",
-      headerName:"# of Products",
-      flex: 0.5,
-      sortable: false,
-      renderCell: (params) => params.value.length // grabing the number of products
-    },
-    {
-      field: "cost",
-      headerName:"Cost",
-      flex: 1,
-      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
-    },
-  
-  ]
-  return (
-    <Box m="1.5rem 2.5rem">
-      <Header title="TRANSACTIONS" subtitle="Entire list of Transactions"/>
-      <Box height="80vh"
-      sx={{
-        "& .MuiDataGrid-root": {
-          border: "none"
+        {
+            field: "_id",
+            headerName: "ID",
+            flex: 1,
         },
-        "& .MuiDataGrid-cell": {
-          borderBottom: "none"
+        {
+            field: "userId",
+            headerName: "User ID",
+            flex: 1,
         },
-        "& .MuiDataGrid": {
-          backgroundColor: theme.palette.background.alt,
-          color: theme.palette.secondary[100],
-          borderBottom: "none"
+        {
+            field: "createdAt",
+            headerName: "Created At",
+            flex: 1,
         },
-        "& .MuiDataGrid-virtualScroller": {
-          backgroundColor: theme.palette.primary.light,
+        {
+            field: "products",
+            headerName: "# of Products",
+            flex: 0.5,
+            sortable: false,
+            renderCell: (params) => params.value.length // Grabbing the number of products
+        },
+        {
+            field: "cost",
+            headerName: "Cost",
+            flex: 1,
+            renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+        },
+    ];
 
-        },
-        "& .MuiDataGrid-footerContainer": {
-          backgroundColor: theme.palette.background.alt,
-          color: theme.palette.secondary[100],
-          borderTop: "none"
-        },
-        "& .MuiDataGrid-toolbarContainer .MuiButton-text":{
-          color: `${theme.palette.secondary[200]} !important`
-        }
-        }}
-      >
-        <DataGrid
-        loading={isLoading || !data}
-        getRowId={(row) => row._id}
-        rows={(data && data.transactions) || {}}
-        columns={columns}
-        rowCount={(data && data.total) || 0}
-        pagination
-        page={page}
-        pageSize={pageSize}
-        paginationMode="server"
-        sortingMode="server"
-        onPageChange={(newPage) => setPage(newPage)} // configuration for settin the new page
-        onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-        onSortModelChange={(newSortModel) => setSort(...newSortModel)}
-        components={{ Toolbar: DataGridCustomToolbar}}
-        />
-      </Box>
-
-    </Box>
-  )
+    return (
+        <Box m="1.5rem 2.5rem">
+            <Header title="TRANSACTIONS" subtitle="Entire list of Transactions" />
+            <Box height="80vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none"
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none"
+                    },
+                    "& .MuiDataGrid": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderBottom: "none"
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: theme.palette.primary.light,
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderTop: "none"
+                    },
+                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                        color: `${theme.palette.secondary[200]} !important`
+                    }
+                }}
+            >
+                <DataGrid
+                    loading={isLoading || !data}
+                    getRowId={(row) => row._id}
+                    rows={(data && data.transactions) || []}
+                    columns={columns}
+                    rowCount={(data && data.total) || 0}
+                    rowsPerPageOptions={[20, 50, 100]}
+                    pagination
+                    page={page}
+                    pageSize={pageSize}
+                    paginationMode="server"
+                    sortingMode="server"
+                    onPageChange={(newPage) => setPage(newPage)} // Configuration for setting the new page
+                    onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                    onSortModelChange={(newSortModel) => setSort(...newSortModel)}
+                    components={{ Toolbar: DataGridCustomToolbar }}
+                    componentsProps={{
+                        toolbar: { searchInput, setSearchInput, setSearch },
+                    }}
+                />
+            </Box>
+        </Box>
+    );
 }
 
-export default Transactions
+export default Transactions;
